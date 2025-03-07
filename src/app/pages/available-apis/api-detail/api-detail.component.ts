97fc49ad013df8f2a1055600038c683dc2fea424
brentham <brent.t.hamilton@gmail.com>
2025-03-07 00:20:25 -0800

    update details

--- src/app/pages/available-apis/api-detail/api-detail.component.ts
+++ src/app/pages/available-apis/api-detail/api-detail.component.ts
@@ -1,37 +1,121 @@
+// // import { Component, OnInit } from '@angular/core';
+// // import { ActivatedRoute, Router } from '@angular/router';
+// // import { ApiEndpoint } from '../../../interfaces/api';
+
 // import { Component, OnInit } from '@angular/core';
 // import { ActivatedRoute, Router } from '@angular/router';
-// import { ApiEndpoint } from '../../../interfaces/api';
-
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { ApiEndpoint, ApiParameter } from '../../../interfaces/api';
-import { Location } from '@angular/common';
-
+// import { ApiEndpoint, ApiParameter } from '../../../interfaces/api';
 // import { Location } from '@angular/common';
-@Component({
-  selector: 'app-api-detail',
-  standalone: false,
-  templateUrl: './api-detail.component.html',
-  styleUrl: './api-detail.component.scss'
-})
+
+// // import { Location } from '@angular/common';
+// @Component({
+//   selector: 'app-api-detail',
+//   standalone: false,
+//   templateUrl: './api-detail.component.html',
+//   styleUrl: './api-detail.component.scss'
+// })
+
+// // export class ApiDetailComponent implements OnInit {
+// //   endpoint: ApiEndpoint | null = null;
+
+// //   constructor(
+// //     private route: ActivatedRoute,
+// //     private router: Router,
+// //     private location: Location
+// //   ) {
+// //     const navigation = this.router.getCurrentNavigation();
+// //     if (navigation?.extras.state) {
+// //       this.endpoint = navigation.extras.state['endpoint'];
+// //     }
+// //   }
+
+// //   ngOnInit() {
+// //     if (!this.endpoint) {
+// //       // If we don't have the endpoint data, redirect back to the list
+// //       this.router.navigate(['/api-list']);
+// //     }
+// //   }
+
+// //   goBack() {
+// //     this.location.back();
+// //   }
+// // }
+
+
+
+// // import { Component, OnInit } from '@angular/core';
+// // import { ActivatedRoute, Router } from '@angular/router';
+// // import { ApiEndpoint, ApiParameter } from '../../../interfaces/api';
+// // import { Location } from '@angular/common';
+
 
 // export class ApiDetailComponent implements OnInit {
 //   endpoint: ApiEndpoint | null = null;
+//   loading = true;
+//   error: string | null = null;
+  
+//   // Add a getter for parameters to ensure we always return an array
+//   get parameters(): ApiParameter[] {
+//     return this.endpoint?.parameters || [];
+//   }
 
 //   constructor(
 //     private route: ActivatedRoute,
 //     private router: Router,
 //     private location: Location
 //   ) {
-//     const navigation = this.router.getCurrentNavigation();
-//     if (navigation?.extras.state) {
-//       this.endpoint = navigation.extras.state['endpoint'];
+//     this.initializeEndpoint();
+//   }
+
+//   // private initializeEndpoint() {
+//   //   try {
+//   //     const navigation = this.router.getCurrentNavigation();
+//   //     if (navigation?.extras.state) {
+//   //       this.endpoint = navigation.extras.state['endpoint'];
+//   //       this.loading = false;
+//   //     } else {
+//   //       // Try getting from route params as fallback
+//   //       const path = this.route.snapshot.paramMap.get('path');
+//   //       const method = this.route.snapshot.paramMap.get('method');
+        
+//   //       if (path && method) {
+//   //         // Could add service call here to fetch endpoint details
+//   //         this.loading = false;
+//   //       } else {
+//   //         throw new Error('No endpoint data available');
+//   //       }
+//   //     }
+//   //   } catch (err) {
+//   //     this.error = 'Failed to load endpoint details';
+//   //     this.loading = false;
+//   //   }
+//   // }
+
+//   private initializeEndpoint() {
+//     try {
+//       const navigation = this.router.getCurrentNavigation();
+//       if (navigation?.extras.state) {
+//         this.endpoint = navigation.extras.state['endpoint'];
+//         this.loading = false;
+//       } else {
+//         // Get the full path from route params
+//         const fullPath = this.route.snapshot.paramMap.get('fullPath');
+//         if (fullPath) {
+//           // Use the full path to fetch endpoint details
+//           // Add your service call here if needed
+//           this.loading = false;
+//         } else {
+//           throw new Error('No endpoint data available');
+//         }
+//       }
+//     } catch (err) {
+//       this.error = 'Failed to load endpoint details';
+//       this.loading = false;
 //     }
 //   }
 
 //   ngOnInit() {
-//     if (!this.endpoint) {
-//       // If we don't have the endpoint data, redirect back to the list
+//     if (!this.endpoint && !this.error) {
 //       this.router.navigate(['/api-list']);
 //     }
 //   }
@@ -39,22 +123,44 @@ import { Location } from '@angular/common';
 //   goBack() {
 //     this.location.back();
 //   }
+
+//   getMethodClass(method: string): string {
+//     return method.toLowerCase();
+//   }
+
+//   isJsonString(str: string): boolean {
+//     try {
+//       JSON.parse(str);
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
+
+//   formatJson(data: any): string {
+//     return JSON.stringify(data, null, 2);
+//   }
 // }
 
 
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ApiEndpoint, ApiParameter } from '../../../interfaces/api';
+import { Location } from '@angular/common';
+import { ApiService } from '../../../services/api.service';
+import { catchError, of } from 'rxjs';
 
-// import { Component, OnInit } from '@angular/core';
-// import { ActivatedRoute, Router } from '@angular/router';
-// import { ApiEndpoint, ApiParameter } from '../../../interfaces/api';
-// import { Location } from '@angular/common';
-
-
+@Component({
+  selector: 'app-api-detail',
+  standalone: false,
+  templateUrl: './api-detail.component.html',
+  styleUrl: './api-detail.component.scss'
+})
 export class ApiDetailComponent implements OnInit {
   endpoint: ApiEndpoint | null = null;
   loading = true;
   error: string | null = null;
   
-  // Add a getter for parameters to ensure we always return an array
   get parameters(): ApiParameter[] {
     return this.endpoint?.parameters || [];
   }
@@ -62,61 +168,48 @@ export class ApiDetailComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private router: Router,
-    private location: Location
+    private location: Location,
+    private apiService: ApiService
   ) {
-    this.initializeEndpoint();
-  }
-
-  // private initializeEndpoint() {
-  //   try {
-  //     const navigation = this.router.getCurrentNavigation();
-  //     if (navigation?.extras.state) {
-  //       this.endpoint = navigation.extras.state['endpoint'];
-  //       this.loading = false;
-  //     } else {
-  //       // Try getting from route params as fallback
-  //       const path = this.route.snapshot.paramMap.get('path');
-  //       const method = this.route.snapshot.paramMap.get('method');
-        
-  //       if (path && method) {
-  //         // Could add service call here to fetch endpoint details
-  //         this.loading = false;
-  //       } else {
-  //         throw new Error('No endpoint data available');
-  //       }
-  //     }
-  //   } catch (err) {
-  //     this.error = 'Failed to load endpoint details';
-  //     this.loading = false;
-  //   }
-  // }
-
-  private initializeEndpoint() {
-    try {
-      const navigation = this.router.getCurrentNavigation();
-      if (navigation?.extras.state) {
-        this.endpoint = navigation.extras.state['endpoint'];
-        this.loading = false;
-      } else {
-        // Get the full path from route params
-        const fullPath = this.route.snapshot.paramMap.get('fullPath');
-        if (fullPath) {
-          // Use the full path to fetch endpoint details
-          // Add your service call here if needed
-          this.loading = false;
-        } else {
-          throw new Error('No endpoint data available');
-        }
-      }
-    } catch (err) {
-      this.error = 'Failed to load endpoint details';
+    // Try to get endpoint from navigation state
+    const navigation = this.router.getCurrentNavigation();
+    if (navigation?.extras.state) {
+      this.endpoint = navigation.extras.state['endpoint'];
       this.loading = false;
     }
   }
 
   ngOnInit() {
-    if (!this.endpoint && !this.error) {
-      this.router.navigate(['/api-list']);
+    // If endpoint wasn't passed via state, try to get it from route params
+    if (!this.endpoint) {
+      // Get the full path from route params
+      const fullPath = this.route.snapshot.paramMap.get('fullPath');
+      
+      if (fullPath) {
+        // Construct the full path with leading slash
+        const path = fullPath.startsWith('/') ? fullPath : '/' + fullPath;
+        
+        this.apiService.getEndpointByPath(path).pipe(
+          catchError(err => {
+            this.error = 'Failed to load endpoint details';
+            this.loading = false;
+            return of(null);
+          })
+        ).subscribe(endpoint => {
+          if (endpoint) {
+            this.endpoint = endpoint;
+            this.loading = false;
+          } else {
+            this.error = 'Endpoint not found';
+            this.loading = false;
+          }
+        });
+      } else {
+        this.error = 'No endpoint information available';
+        this.loading = false;
+        // Navigate back to the list after a short delay
+        setTimeout(() => this.router.navigate(['/api-list']), 2000);
+      }
     }
   }
 
--- src/app/pages/available-apis/api-list/api-list.component.ts
+++ src/app/pages/available-apis/api-list/api-list.component.ts
@@ -1,12 +1,108 @@
+// import { Component, OnInit } from '@angular/core';
+// import { ActivatedRoute, Router } from '@angular/router';
+// import { ApiService } from '../../../services/api.service';
+// import { ApiEndpoint } from '../../../interfaces/api';
+// import { Observable, catchError, map, of, startWith } from 'rxjs';
+// import { MatTableModule } from '@angular/material/table';
+// import { MatChipsModule } from '@angular/material/chips';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatIconModule } from '@angular/material/icon';
+
+// interface LoadingState<T> {
+//   data: T | null;
+//   loading: boolean;
+//   error: string | null;
+// }
+
+// @Component({
+//   selector: 'app-api-list',
+//   standalone: false,
+//   templateUrl: './api-list.component.html',
+//   styleUrl: './api-list.component.scss'
+// })
+// export class ApiListComponent implements OnInit {
+//   state$!: Observable<LoadingState<ApiEndpoint[]>>;
+
+//   constructor(
+//     private apiService: ApiService,
+//     private router: Router,
+//     private route: ActivatedRoute
+//   ) {}
+
+//   ngOnInit() {
+//     this.loadEndpoints();
+//   }
+
+//   loadEndpoints() {
+//     this.state$ = this.apiService.getApiEndpoints().pipe(
+//       map(data => ({
+//         data,
+//         loading: false,
+//         error: null
+//       })),
+//       catchError(error => of({
+//         data: null,
+//         loading: false,
+//         error: 'Failed to load API endpoints. Please try again later.'
+//       })),
+//       startWith({
+//         data: null,
+//         loading: true,
+//         error: null
+//       })
+//     );
+//   }
+
+//   // showEndpointDetails(endpoint: ApiEndpoint) {
+//   //   // Encode the path to handle special characters
+//   //   const encodedPath = encodeURIComponent(endpoint.path);
+//   //   this.router.navigate(['/api-details', encodedPath], {
+//   //     state: { endpoint }
+//   //   });
+//   // }
+
+//   // showEndpointDetails(endpoint: ApiEndpoint) {
+//   //   // Clean the path
+//   //   let path = endpoint.path;
+    
+//   //   // Remove leading slash if present
+//   //   if (path.startsWith('/')) {
+//   //     path = path.slice(1);
+//   //   }
+
+//   //   // Navigate using route segments
+//   //   const segments = ['api-details', ...path.split('/')];
+    
+//   //   this.router.navigate(segments, {
+//   //     state: { endpoint },
+//   //     // Preserve the query params and fragment if any
+//   //     preserveFragment: true,
+//   //     queryParamsHandling: 'preserve'
+//   //   });
+//   // }
+
+
+//   showEndpointDetails(endpoint: ApiEndpoint) {
+//     // Remove leading slash if present
+//     const path = endpoint.path.startsWith('/') ? endpoint.path.slice(1) : endpoint.path;
+    
+//     // Create the route segments array
+//     const segments = ['api-details', ...path.split('/')];
+    
+//     this.router.navigate(segments, {
+//       state: { endpoint },
+//       relativeTo: this.route // Add this if you want relative navigation
+//     });
+//   }
+// }
+
+
+
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ApiService } from '../../../services/api.service';
 import { ApiEndpoint } from '../../../interfaces/api';
 import { Observable, catchError, map, of, startWith } from 'rxjs';
-import { MatTableModule } from '@angular/material/table';
-import { MatChipsModule } from '@angular/material/chips';
-import { MatButtonModule } from '@angular/material/button';
-import { MatIconModule } from '@angular/material/icon';
 
 interface LoadingState<T> {
   data: T | null;
@@ -53,45 +149,19 @@ export class ApiListComponent implements OnInit {
     );
   }
 
-  // showEndpointDetails(endpoint: ApiEndpoint) {
-  //   // Encode the path to handle special characters
-  //   const encodedPath = encodeURIComponent(endpoint.path);
-  //   this.router.navigate(['/api-details', encodedPath], {
-  //     state: { endpoint }
-  //   });
-  // }
-
-  // showEndpointDetails(endpoint: ApiEndpoint) {
-  //   // Clean the path
-  //   let path = endpoint.path;
-    
-  //   // Remove leading slash if present
-  //   if (path.startsWith('/')) {
-  //     path = path.slice(1);
-  //   }
-
-  //   // Navigate using route segments
-  //   const segments = ['api-details', ...path.split('/')];
-    
-  //   this.router.navigate(segments, {
-  //     state: { endpoint },
-  //     // Preserve the query params and fragment if any
-  //     preserveFragment: true,
-  //     queryParamsHandling: 'preserve'
-  //   });
-  // }
-
-
   showEndpointDetails(endpoint: ApiEndpoint) {
-    // Remove leading slash if present
+    // Remove leading slash if present to avoid double slashes
     const path = endpoint.path.startsWith('/') ? endpoint.path.slice(1) : endpoint.path;
     
     // Create the route segments array
     const segments = ['api-details', ...path.split('/')];
     
+    // Use the router to navigate to the detail page,
+    // also passing the endpoint object through the router state
     this.router.navigate(segments, {
       state: { endpoint },
-      relativeTo: this.route // Add this if you want relative navigation
+      // Navigate relative to the parent route (available-apis)
+      relativeTo: this.route.parent
     });
   }
 }
